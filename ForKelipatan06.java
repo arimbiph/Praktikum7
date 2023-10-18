62624b950d4e46cfa3e1c62307a5fc8fd1895932
arimbiph <putriarimbi714@gmail.com>
2023-10-18 12:51:52 +0700

    modifikasi1

--- ForKelipatan06.java
+++ ForKelipatan06.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 public class ForKelipatan06{
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner input06 = new Scanner(System.in);
+        double rataRata;
+        int bilangan, total, counter;
+
+        total = 0;
+        counter = 0;
         
-        int bilangan, total = 0, counter = 0;
-        System.out.print("Masukkan bilangan 1-9: ");
-        bilangan = scan.nextInt();
+        System.out.print("Masukkan bilangan kelipatan 1-9: ");
+        bilangan = input06.nextInt();
 
         for (int i = 0; i < 50; i++) {
             if (i % bilangan == 0){
@@ -13,6 +17,13 @@ public class ForKelipatan06{
                 counter++;
       }
    }
-        System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d", counter, bilangan, total);5  
-}
+if (counter > 0) {
+rataRata = (double) total / counter;
+
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", bilangan , counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah  %d\n", bilangan, total);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n,", bilangan, rataRata);
+       
+        }
+    }
 }
